a5efb5020e5472d40f85b200e6a4ca937271a5ab
Steven Sierra F <steven@hawking.local>
2018-05-21 00:05:45 -0500

    First stable release

--- provision/provision.sh
+++ provision/provision.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
 
-
-
-
 if [ -z $1 ] && [ -z $2 ]; then
   echo "Debe ingresar un host para instalar el servicio de metricas"
   exit 1
@@ -11,6 +8,14 @@ fi
 if [ -z "$(command -v ansible-playbook)" ]; then
    echo "Para provisionar debera instalar previamente ansible-playbook"
    echo "Consulte como instalar ansible-playbook aqui: "
+   exit 1
+fi
+
+if [ ! -z $3 ]; then
+    echo "Provisionando $2 con metricbeat y collectd"
+    ansible-playbook -u vagrant -i "$1," -e "node_name=$2" -e "hostname=$2" ansible/main.yml
+    exit 0
 else
-  ansible-playbook -u vagrant -i "$1," -e "node_name=$2" --tags "metricbeat" ansible/main.yml
+  echo "Provisionando $2 con metricbeat"
+  ansible-playbook -u vagrant -i "$1," -e "node_name=$2" --tags "beats" ansible/main.yml
 fi
